360a93b9ac2be02e9994c630bcb923504e01c19f
hayden <hayden.ismet@amigotechnology.com>
2022-07-06 20:15:40 +0100

    editing reg view one

--- src/components/login-and-signup/LoginOrRegisterView.js
+++ src/components/login-and-signup/LoginOrRegisterView.js
@@ -21,36 +21,7 @@ export default function LoginOrRegisterView(props) {
       <div className="arc_container_reg">
         <RegistrationLoginHeaderElement />
         <div className="arc_login">
-          <GenericInput
-            inputCategory="Email"
-            inputType="email"
-            labelName="arc_email_login_label"
-            view={view}
-            inputTypeScrape={inputTypeScrape}
-            onGenericInputChange={onGenericInputChange}
-            input={input}
-            userEmail={userEmail}
-          />
-          <GenericInput
-            inputCategory="Password"
-            inputType="password"
-            labelName="arc_password_login_label"
-            view={view}
-            inputTypeScrape={inputTypeScrape}
-            onGenericInputChange={onGenericInputChange}
-            input={input}
-            userPassword={userPassword}
-          />
-          <div className="arc_forgot_password">forgot password?</div>
-          <LoginButton
-            input={input}
-            inputTypeScrape={inputTypeScrape}
-            onGenericInputChange={onGenericInputChange}
-            userEmail={userEmail}
-            userPassword={userPassword}
-          />
           <div className="arc_new_account">
-            <div className="arc_no_account"> don't have an account?</div>
             <NextElement
               nextCopy="create a new account"
               handleViewClickNext={handleViewClickNext}
