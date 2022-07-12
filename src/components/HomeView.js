754c254ca19d353ddad458b3579597e20ab6b299
hayden <hayden.ismet@amigotechnology.com>
2022-07-12 19:35:31 +0100

    md5

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { RegistrationContext } from "./context/RegistrationContext.context";
 import HomeHeader from "./elements/HomeHeader";
 import PetCard from "./elements/pet-cards/PetCard";
+import md5 from "md5";
 
 export default function HomeView(props) {
   const context = useContext(RegistrationContext);
 
   console.log(context.registerAccount);
+  console.log(md5("ello"));
 
   return (
     <>
