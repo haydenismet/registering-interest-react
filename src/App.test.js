263752039d2387910b721fac875a5381fcc1364b
hayden <hayden.ismet@amigotechnology.com>
2022-09-21 12:24:30 +0100

    refactor tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,52 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { RegistrationProvider } from "./components/context/RegistrationContext.context.js";
 import TermsAndConditionsView from "./components/pages/TermsAndConditionsView.js";
 import App from "./containers/App.js";
 
-test("Landing Page Assertions", () => {
+let nameInput = (labelValue) => {
+  const input = screen.getByLabelText(labelValue);
+  return {
+    input,
+  };
+};
+
+const landingButtons = () => {
+  const registerButton = screen.getByText("Register");
+  return {
+    registerButton,
+  };
+};
+
+const termsAndConditionsButtons = () => {
+  const agreeButton = screen.getByText("Agree");
+  return {
+    agreeButton,
+  };
+};
+
+const customerTypeButtons = () => {
+  const selectOption = screen.getByLabelText("Commercial");
+  const continueButton = screen.getByText("continue");
+  return {
+    selectOption,
+    continueButton,
+  };
+};
+
+test("Landing Page Assertions", async () => {
   render(
     <RegistrationProvider>
       <App />
     </RegistrationProvider>
   );
-  const registerButton = screen.getByText("Register");
+  const { registerButton } = landingButtons();
   expect(screen.getByText("Knight Cat")).toHaveClass("arc_header_knightcat");
   expect(screen.getByText("Register")).not.toBeDisabled();
   userEvent.click(registerButton);
-  expect(screen.getByText("Disclaimer.")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByText("Disclaimer.")).toBeInTheDocument();
+  });
 });
 
 test("Terms and Conditions Page Assertions", () => {
@@ -23,7 +55,7 @@ test("Terms and Conditions Page Assertions", () => {
       <App />
     </RegistrationProvider>
   );
-  const registerButton = screen.getByText("Register");
+  const { registerButton } = landingButtons();
   userEvent.click(registerButton);
   expect(
     screen.getByText(
@@ -32,44 +64,65 @@ test("Terms and Conditions Page Assertions", () => {
   ).toHaveClass("arc_t_c_h5");
   expect(screen.getByText("Back")).not.toBeDisabled();
   expect(screen.getByText("Agree")).not.toBeDisabled();
-  const agreeButton = screen.getByText("Agree");
+  const { agreeButton } = termsAndConditionsButtons();
   userEvent.click(agreeButton);
-  const selectOption = screen.getByLabelText("Commercial");
+  const { selectOption } = customerTypeButtons();
   expect(screen.getByLabelText("Commercial")).toBeInTheDocument();
   expect(screen.getByLabelText("Homeowner")).toBeInTheDocument();
   expect(screen.getByText("continue")).toBeDisabled();
   userEvent.click(selectOption);
   expect(screen.getByText("continue")).not.toBeDisabled();
-  const continueButton = screen.getByText("continue");
+  const { continueButton } = customerTypeButtons();
   userEvent.click(continueButton);
   expect(
     screen.getByText(/A few more steps to secure priority access to /)
   ).toBeInTheDocument();
 });
 
-test("Input assertions", () => {
+test("Input Name", () => {
   render(
     <RegistrationProvider>
       <App />
     </RegistrationProvider>
   );
-  const registerButton = screen.getByText("Register");
+  const { registerButton } = landingButtons();
   userEvent.click(registerButton);
-  const agreeButton = screen.getByText("Agree");
+  const { agreeButton } = termsAndConditionsButtons();
   userEvent.click(agreeButton);
-  const selectOption = screen.getByLabelText("Commercial");
+  const { selectOption } = customerTypeButtons();
   userEvent.click(selectOption);
-  const continueButton = screen.getByText("continue");
+  const { continueButton } = customerTypeButtons();
   userEvent.click(continueButton);
 
-  expect(screen.getByLabelText("Organisation Name")).toHaveClass(
-    "arc_generic_input Organisation Name"
-  );
+  let { input } = nameInput("Organisation Name");
+  fireEvent.change(input, { target: { value: "Hayden Ismet Industries" } });
+  expect(input.value).toBe("Hayden Ismet Industries");
+});
 
-  expect(screen.getByLabelText("Email")).toHaveClass("arc_generic_input Email");
+test("Input Email", () => {
+  render(
+    <RegistrationProvider>
+      <App />
+    </RegistrationProvider>
+  );
+  const { registerButton } = landingButtons();
+  userEvent.click(registerButton);
+  const { agreeButton } = termsAndConditionsButtons();
+  userEvent.click(agreeButton);
+  const { selectOption } = customerTypeButtons();
+  userEvent.click(selectOption);
+  const { continueButton } = customerTypeButtons();
+  userEvent.click(continueButton);
+
+  let { input } = nameInput("Email");
+  fireEvent.change(input, { target: { value: "h@h.com" } });
+  expect(input.value).toBe("h@h.com");
 });
 
 // add a reverse of traversing the back buttons
 // set up buttons with like a beforeeach?
 // loops or length checks to avoid repetition of expects?
 // utilise Jest and react testing library in tandem?
+// utilisation of await async?
+//const emailInput = screen.getByLabelText("Email");
+//userEvent.click(emailInput);
